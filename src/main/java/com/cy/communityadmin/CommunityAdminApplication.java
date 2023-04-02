5c92e0c7a63677121e24aac56c8c4ef674377d32
striocy <1871208286@qq.com>
2023-04-02 21:38:18 +0800

    Initial commit

---
+++ src/main/java/com/cy/communityadmin/CommunityAdminApplication.java
@@ -0,0 +1,13 @@
+package com.cy.communityadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunityAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommunityAdminApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cy/communityadmin/CommunityAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cy.communityadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
